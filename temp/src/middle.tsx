21662d9458cbb9d88e3e6dac07dbf132efd25b64
Joey Issa <joeyissa59@gmail.com>
2024-09-22 15:10:35 -0400

    blurbs

--- temp/src/middle.tsx
+++ temp/src/middle.tsx
@@ -1,33 +1,26 @@
 import React from 'react';
 
+const Blurb = () => {
+    return (
+        <div>
+            <div className="widget rounded-md p-3 m-2">
+                <h1 className="text-3xl font-bold">About Me</h1>
+                <p className="">I am a software engineer with a focus on web development. I have experience with React, Node.js, and Python. I am also familiar with databases such as MySQL and MongoDB. I am currently looking for a job.</p>
+            </div>
+        </div>
+    );
+}
+
 const Middle = () => {
     return (
         <div className='block w-[580px]'>
             <div className='pt-[80px] pb-[50px]'>
-                <div className="widget rounded-md p-3 m-2">
-                    <h1 className="text-3xl font-bold">About Me</h1>
-                    <p className="">I am a software engineer with a focus on web development. I have experience with React, Node.js, and Python. I am also familiar with databases such as MySQL and MongoDB. I am currently looking for a job in web development.</p>
-                </div>
-                <div className="widget rounded-md p-3 m-2">
-                    <h1 className="text-3xl font-bold">About Me</h1>
-                    <p className="">I am a software engineer with a focus on web development. I have experience with React, Node.js, and Python. I am also familiar with databases such as MySQL and MongoDB. I am currently looking for a job in web development.</p>
-                </div>
-                <div className="widget rounded-md p-3 m-2">
-                    <h1 className="text-3xl font-bold">About Me</h1>
-                    <p className="">I am a software engineer with a focus on web development. I have experience with React, Node.js, and Python. I am also familiar with databases such as MySQL and MongoDB. I am currently looking for a job in web development.</p>
-                </div>
-                <div className="widget rounded-md p-3 m-2">
-                    <h1 className="text-3xl font-bold">About Me</h1>
-                    <p className="">I am a software engineer with a focus on web development. I have experience with React, Node.js, and Python. I am also familiar with databases such as MySQL and MongoDB. I am currently looking for a job in web development.</p>
-                </div>
-                <div className="widget rounded-md p-3 m-2">
-                    <h1 className="text-3xl font-bold">About Me</h1>
-                    <p className="">I am a software engineer with a focus on web development. I have experience with React, Node.js, and Python. I am also familiar with databases such as MySQL and MongoDB. I am currently looking for a job in web development.</p>
-                </div>
-                <div className="widget rounded-md p-3 m-2">
-                    <h1 className="text-3xl font-bold">About Me</h1>
-                    <p className="">I am a software engineer with a focus on web development. I have experience with React, Node.js, and Python. I am also familiar with databases such as MySQL and MongoDB. I am currently looking for a job in web development.</p>
-                </div>
+                <Blurb />
+                <Blurb />
+                <Blurb />
+                <Blurb />
+                <Blurb />
+                <Blurb />
             </div>
         </div>
     );
